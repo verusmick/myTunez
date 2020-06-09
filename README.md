b19d4d1412403b3114a2917f8229e08ccd9f1daa
verusmick <miguel.angel.angulo72@gmail.com>
2020-06-09 09:45:49 -0400

    Add main structure

---
+++ README.md
@@ -0,0 +1,8 @@
+   change directory:
+     $ cd myTunez
+
+   install dependencies:
+     $ npm install
+
+   run the app:
+     $ DEBUG=mytunez:* npm start
---
+++ routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const db = require('../bd/connection')
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // res.send('respond with a resource');
+
+  
+  
+  db.query('SELECT * FROM  user', function (err, results) {
+    if (err) throw err
+    return res.json(results)
+  })
+});
+
+module.exports = router;
